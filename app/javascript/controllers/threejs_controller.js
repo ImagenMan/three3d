2f18feaf3a186a4a05da98d82d6a818f172d3587
Dave <psychostasia1@gmail.com>
2022-09-25 13:52:32 -0500

    added rotation frames

--- app/javascript/controllers/threejs_controller.js
+++ app/javascript/controllers/threejs_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
-import * THREE from "three";
+import { Controller } from "@hotwired/stimulus";
+import * as THREE from "three";
 
 // Connects to data-controller="threejs"
 export default class extends Controller {
@@ -7,5 +7,42 @@ export default class extends Controller {
     console.log("hello stimulus", this.element);
 
     this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    this.renderer = new THREE.WebGL1Renderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement)
+
+    this.geometry = new THREE.BoxGeometry();
+    this.material = new THREE.MeshBasicMaterial({ 
+      color: 0x00ff00, 
+      wireframe: true,
+     });
+
+     this.originCube = this.creatCube(0, 0, 0);
+     this.scene.add(this.originCube);
+     this.camera.position.z = 5;
+     this.animate();
+    }
+
+    animate() {
+      requestAnimationFrame(this.animate.bind(this));
+
+      this.originCube.rotation.x += 0.01;
+      this.originCube.rotation.y += 0.01;
+
+      this.renderer.render(this.scene, this.camera);
+    }
+
+  creatCube(x, y, z) {
+    const cube = new THREE.Mesh(this.geometry, this.material);
+    cube.position.set(x, y, z);
+    
+    return cube;
   }
 }
